c8cd5d47d6016aebf73824836c30fe23a8779926
Raiza04 <130138991+Raiza04@users.noreply.github.com>
2026-06-29 20:22:28 +0200

    Change convert function to use double type
    Updated the convert function to accept a double instead of a long int and modified the main function to test with a double value.

--- arian5.c
+++ arian5.c
@@ -3,16 +3,19 @@
 #include <assert.h>
 
 // range of 16 bit int => -32768 to 32767
+// short int => 16 bits
+// double => 64 bits
 
-void convert(long int x){
-	assert(x >= -32768 && x <= 32767);
-	short int y = (short int)x;
-	printf("y = %hd \n", y);
+void convert(double x){
+        //assert(x >= -32768 && x <= 32767); //uncomment to try assert
+        short int y = x;
+        printf("y = %hd \n", y);
 }
 
 int main() {
-	long int x = 40000; // Is over 32767 => Integer Overflow
-	convert(x);
-	return 0;
-}
+        double x = 35000.9999; // Is over 32767 => Integer Overflow
+        convert(x);
 
+        //static_assert(1==0); //uncomment to try static_assert
+        return 0;
+}
